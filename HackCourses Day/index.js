af4f8537de73e5ea347a6ffbf160f093690e87de
GAVHANE MAYUR RAJENDRA <2020bec075@sggs.ac.in>
2023-06-17 15:21:58 +0530

    mayur
    mayur

---
+++ HackCourses Day/index.js
@@ -0,0 +1,40 @@
+
+
+
+// text animation
+
+
+  var typed = new Typed(".auto-typing",{
+    strings : ["Programming Courses"," Designing Courses", "Hacking Courses", "Udemy Courses", "Coursera",  "11tH-12TH Courses", "Gate Courses", "Medical Courses" ,"UPSC courses"
+        ,"Development Courses"],
+    typeSpeed : 50,
+    backSpeed : 50,
+    looped : Infinity
+  })
+
+ 
+
+    // hamburger = document.querySelector(".br-btn");
+    //     nav = document.querySelector("nav");
+    //     hamburger.onclick = function() {
+    //         nav.classList.toggle("active");
+    //     }
+
+
+    let listmenu = document.querySelector('.list')
+      
+
+
+
+
+
+        
+
+  function searchcourse() {
+  let filter = document.getElementById('myinput').value.toUpperCase();
+  let ul = document.getElementById('course-content');
+  let li = ul.getElementsByTagName('h2');
+
+
+}
+ 
